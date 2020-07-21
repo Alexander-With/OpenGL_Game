e9b874fe851c551bd2920e457f971d31bbba16e8
Alexander With <alexander@a-with.dk>
2020-07-21 20:50:59 +0200

    Added grid lines

--- main.cpp
+++ main.cpp
@@ -15,36 +15,46 @@
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void windowCloseCallback(GLFWwindow* window);
 void processInput(GLFWwindow* window);
+
+void createRectObject();
 void writeRect(int shaderProgram, int gridSize);
+
+void createLineObject();
+void writeLine(int shaderProgram, int gridSize);
 
 const char* vertexShaderSource = 
 	"#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
 	"layout (location = 1) in vec3 aColor;\n"
-	// "layout (location = 2) in vec2 aTexCoord;\n"
+	"layout (location = 2) in vec2 aTexCoord;\n"
 	"out vec3 color;\n"
-	// "out vec2 texCoord;\n"
+	"out vec2 texCoord;\n"
 	"uniform mat4 transform;\n"
 	"void main(){\n"
 	"	gl_Position = transform * vec4(aPos, 1.0);\n"
 	"	color = aColor;\n"
-	// "	texCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
+	"	texCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
 	"}\n"
 ;
 
 const char* fragmentShaderSource =
 	"#version 330 core\n"
+	"out vec4 FragColor;\n"
 	"in vec3 color;\n"
 	"in vec2 texCoord;\n"
-	"out vec4 FragColor;\n"
-	"uniform sampler2D texture;\n"
+	"uniform sampler2D tex;\n"
 	"void main(){\n"
 	"	FragColor = vec4(color, 1.0f);\n"
-	// "	FragColor = texture(texture, texCoord);\n"
+	"	FragColor = texture(tex, texCoord);\n"
 	"}\n"
 ;
 
 unsigned int VAO, VBO, EBO;
+unsigned int VAOgrid, VBOgrid, EBOgrid;
+unsigned int VAOline, VBOline, EBOline;
+
+unsigned int texture;
+int gridSize = 10;
 
 bool rerun = 1;
 bool rotate = 0;
@@ -130,73 +140,14 @@ int main(){
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	// // Graphics Data
-	// float vertices[] = {
-	// 	-1.0f, 1.0f, 0.0f,		0.0f, 0.75f, 1.0f,	1.0f, 1.0f,
-	// 	-1.0f, 0.0f, 0.0f,		0.0f, 0.75f, 1.0f,	1.0f, 0.0f,
-	// 	0.0f, 0.f, 0.0f,		0.0f, 0.75f, 1.0f,	0.0f, 0.0f,
-	// 	0.0f, 1.0f, 0.0f,		0.0f, 0.75f, 1.0f,	0.0f, 1.0f
-	// };
-
-	// unsigned int indices[] = {
-	// 	0, 1, 3,
-	// 	1, 2, 3
-	// };
-
-
-	int gridSize = 1000;
+	//------------------------------------------------------------------------ 
 
 	// // Vertex Objects
-	// unsigned int VAO, VBO, EBO;
-
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-
-	// // OpenGL Object
-	glBindVertexArray(VAO);
-	
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, gridSize*gridSize*4*8*sizeof(float), NULL, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, gridSize*gridSize*6*sizeof(unsigned int), NULL, GL_STATIC_DRAW);
-	
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-
-	// // OpenGL Texture
-	// unsigned int texture;
-	// glGenTextures(1, &texture);
-	// glBindTexture(GL_TEXTURE_2D, texture);
-
-	// // Texture Wrapping
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-
-	// // Texture Filtering
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	// // Load Image
-	// int width, height, nrChannels;
-	// unsigned char* data = stbi_load("../wall.jpg", &width, &height, &nrChannels, 0);
-	// if(data){
-	// 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-	// 	glGenerateMipmap(GL_TEXTURE_2D);
-	// }
-	// else{
-	// 	printf("Failed to load texture");
-	// }
-	// stbi_image_free(data);
-
-	glBindVertexArray(0);
+	createRectObject();
+	createLineObject();
 
 	// writeRect(shaderProgram, gridSize);
+	writeLine(shaderProgram, gridSize);
 
 	float timeOld = 0;
 	float timeValue = 0;
@@ -214,6 +165,16 @@ int main(){
 		timeValue = glfwGetTime();
 
 		glUseProgram(shaderProgram);
+		glm::mat4 trans = glm::mat4(1.0f);
+		if(rotate){
+			trans = glm::rotate(trans, glm::radians(-45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+			trans = glm::rotate(trans, glm::radians(-60.0f), glm::vec3(1.0f, 1.0f, 0.0f));
+		}
+		trans = glm::translate(trans, glm::vec3(-0.6f, -0.55f, 0.0f));
+		trans = glm::scale(trans, glm::vec3(0.15f, 0.15f, 0.0f));
+
+		unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
 		
 
@@ -222,25 +183,17 @@ int main(){
 			writeRect(shaderProgram, gridSize);
 			rerun = 0;
 		}
-		
-		glBindVertexArray(VAO);
-
-		// glBindTexture(GL_TEXTURE_2D, texture);
 
 
-		glm::mat4 trans = glm::mat4(1.0f);
-		if(rotate){
-			trans = glm::rotate(trans, glm::radians(-45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-			trans = glm::rotate(trans, glm::radians(-60.0f), glm::vec3(1.0f, 1.0f, 0.0f));
-		}
-		trans = glm::translate(trans, glm::vec3(-1.0f, -1.0f, 0.0f));
-		trans = glm::scale(trans, glm::vec3(0.05f, 0.05f, 0.0f));
-
-		unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-
+		// Draw Grid Tiles
+		glBindVertexArray(VAOgrid);
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glDrawElements(GL_TRIANGLES, 6*gridSize*gridSize, GL_UNSIGNED_INT, 0);
+		glBindTexture(GL_TEXTURE_2D, 0);
 
+		// Draw Grid Lines
+		glBindVertexArray(VAOline);
+		glDrawElements(GL_LINES, 4*gridSize*gridSize, GL_UNSIGNED_INT, 0);
 
 		// Events and Swap
 		glfwSwapBuffers(window);
@@ -304,22 +257,70 @@ void processInput(GLFWwindow* window){
 	}
 }
 
+void createRectObject(){
+	glGenVertexArrays(1, &VAOgrid);
+	glGenBuffers(1, &VBOgrid);
+	glGenBuffers(1, &EBOgrid);
+
+	// // OpenGL Object
+	glBindVertexArray(VAOgrid);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBOgrid);
+	glBufferData(GL_ARRAY_BUFFER, gridSize * gridSize * 4 * 8 * sizeof(float), NULL, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOgrid);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, gridSize * gridSize * 6 * sizeof(unsigned int), NULL, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	// OpenGL Texture
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// Texture Wrapping
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+
+	// Texture Filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Load Image
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load("../textures/grass02.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		printf("Failed to load texture");
+	}
+	stbi_image_free(data);
+
+	glBindVertexArray(0);
+}
+
 void writeRect(int shaderProgram, int gridSize){
-	glBindVertexArray(VAO);
-	// char *verticesPtr = (char*)glMapBufferRange(GL_ARRAY_BUFFER, 0, (sizeof(float)*8*4*gridSize*gridSize), GL_MAP_WRITE_BIT);
-	// char *indicesPtr = (char*)glMapBufferRange(GL_ELEMENT_ARRAY_BUFFER, 0, (sizeof(int))*6*gridSize*gridSize, GL_MAP_WRITE_BIT);
+	glBindVertexArray(VAOgrid);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &VBOgrid);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	glDeleteBuffers(1, &EBO);
+	glDeleteBuffers(1, &EBOgrid);
 
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glGenBuffers(1, &VBOgrid);
+	glGenBuffers(1, &EBOgrid);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOgrid);
 	glBufferData(GL_ARRAY_BUFFER, gridSize*gridSize*4*8*sizeof(float), NULL, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOgrid);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, gridSize*gridSize*6*sizeof(unsigned int), NULL, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -346,6 +347,95 @@ void writeRect(int shaderProgram, int gridSize){
 			unsigned int indices[] = {
 				0+square*4, 1+square*4, 3+square*4,
 				1+square*4, 2+square*4, 3+square*4
+			};
+
+			memcpy(verticesPtr, vertices, sizeof(vertices));
+			verticesPtr += sizeof(vertices);
+
+			memcpy(indicesPtr, indices, sizeof(indices));
+			indicesPtr += sizeof(indices);
+		}
+	}
+
+	glUnmapBuffer(GL_ARRAY_BUFFER);
+	glUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
+	glBindVertexArray(0);
+}
+
+void createLineObject(){
+	glGenVertexArrays(1, &VAOline);
+	glGenBuffers(1, &VBOline);
+	glGenBuffers(1, &EBOline);
+
+	// // OpenGL Object
+	glBindVertexArray(VAOline);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBOline);
+	glBufferData(GL_ARRAY_BUFFER, gridSize*gridSize*4*6*sizeof(float), NULL, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOline);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, gridSize*gridSize*4*sizeof(unsigned int), NULL, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void *)(3*sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(0);
+}
+
+void writeLine(int shaderProgram, int gridSize){
+	glBindVertexArray(VAOline);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &VBOline);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &EBOline);
+
+	glGenBuffers(1, &VBOline);
+	glGenBuffers(1, &EBOline);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOline);
+	glBufferData(GL_ARRAY_BUFFER, gridSize*gridSize*4*6*sizeof(float), NULL, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOline);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, gridSize*gridSize*4*sizeof(unsigned int), NULL, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+
+	char *verticesPtr = (char*)glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+	char *indicesPtr = (char*)glMapBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY);
+
+	for(int y = 0; y < gridSize; y++){
+		for(int x = 0; x < gridSize; x++){
+			unsigned int square = (gridSize*y+x);
+			float vertices[] = {
+				0.5f+x, 0.5f+y, 0.0f,		0.0f, 0.0f, 0.0f,
+				0.5f+x, -0.5f+y, 0.0f,		0.0f, 0.0f, 0.0f
+			};
+
+			unsigned int indices[] = {
+				0+square*2, 1+square*2
+			};
+
+			memcpy(verticesPtr, vertices, sizeof(vertices));
+			verticesPtr += sizeof(vertices);
+
+			memcpy(indicesPtr, indices, sizeof(indices));
+			indicesPtr += sizeof(indices);
+		}
+	}
+
+	for(int y = 0; y < gridSize; y++){
+		for(int x = 0; x < gridSize; x++){
+			unsigned int square = (gridSize*y+x);
+			float vertices[] = {
+				0.5f+x, 0.5f+y, 0.0f,		0.0f, 0.0f, 0.0f,
+				-0.5f+x, 0.5f+y, 0.0f,		0.0f, 0.0f, 0.0f
+			};
+
+			unsigned int indices[] = {
+				(2*gridSize*gridSize)+0+square*2, (2*gridSize*gridSize)+1+square*2
 			};
 
 			memcpy(verticesPtr, vertices, sizeof(vertices));
